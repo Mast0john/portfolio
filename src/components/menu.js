8e8e87a2887d22caa11a3444946a6cd12b1e8f7c
Mast0john <76588199+Mast0john@users.noreply.github.com>
2022-05-19 01:18:02 +0200

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -274,7 +274,7 @@ const Menu = () => {
               Resume&nbsp;
               <a
                 className="resume-text-button"
-                href="../../static/%5BFR%5Dresume.pdf"
+                href="/%5BFR%5Dresume.pdf"
                 target="_blank"
                 rel="noopener noreferrer">
                 FR
@@ -284,7 +284,7 @@ const Menu = () => {
               </label>
               <a
                 className="resume-text-button"
-                href="../../static/%5BEN%5Dresume.pdf"
+                href="/%5BEN%5Dresume.pdf"
                 target="_blank"
                 rel="noopener noreferrer">
                 EN
